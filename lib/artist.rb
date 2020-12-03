567e88750ef56ce94b8ef747c9a23d07030f0085
Chandler Craig <hellochandlercraig@gmail.com>
2020-12-03 18:47:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,6 +14,11 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end
 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
+  end
+
   def self.song_count
   end
 
